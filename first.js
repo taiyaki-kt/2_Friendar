1d16b93b2067a0c4e2816df2089a59a2f3ffc4e6
rrzjntwsb7-web <rrzjntwsb7@privaterelay.appleid.com>
2026-08-19 16:54:20 +0900

    カレンダー追加

--- first.js
+++ first.js
@@ -147,7 +147,7 @@ joinTeamButton.addEventListener("click", async () => {
 
     console.log("チームに参加しました！");
     console.log("あなたのUID:", user.uid);
-
+    window.location.href = "./calendar.html";
 } else {
 
     console.log("チームが見つかりません");
